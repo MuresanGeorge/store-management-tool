0b626dc0ee6fc741f0d5fcf2d7ab767e7c140875
MuresanGeorge <muresangeorge1996@gmail.com>
2025-04-06 20:26:49 +0300

    i added APIs for getProduct, getProductByCategory, addProduct, logging and i've imlpemented add category service

--- src/main/java/com/inghub/store/repository/ProductRepository.java
+++ src/main/java/com/inghub/store/repository/ProductRepository.java
@@ -4,10 +4,13 @@ import com.inghub.store.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findByName(String name);
+
+    List<Product> findByCategoryId(Long id);
 }
